0f00d915027fc771f1c2cfb79cd257b9a13a8543
Sumit Vishwkarma <justmultiplythinks@gmail.com>
2024-07-08 20:13:50 +0530

    Terminal size changed

--- lua/config/keybinding.lua
+++ lua/config/keybinding.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>tt", ":ToggleTerm<CR> ", {})
+vim.keymap.set("n", "<leader>tt", ":ToggleTerm size=15<CR> ", {})
 vim.keymap.set("n", "<leader>ty", ":ToggleTerm direction=float <CR> ", {})
 
 vim.keymap.set("t", "<leader>tt", "exit <CR> ", {})
