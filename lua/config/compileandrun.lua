59dfd9c952be9d51de7113fe952d5357d72eef4f
Sumit Vishwkarma <justmultiplythinks@gmail.com>
2024-07-10 00:28:09 +0530

    Added cpp compiler and run shortcut

--- lua/config/compileandrun.lua
+++ lua/config/compileandrun.lua
@@ -1,8 +1,17 @@
 function CompileAndRun()
 	if vim.bo.filetype == "c" then
 		local output = vim.fn.expand("%:r")
-		vim.cmd("w")                                 -- Save the file
-		vim.cmd("!gcc % -o " .. output)              -- Compile the file
+		vim.cmd("w")                                   -- Save the file
+		vim.cmd("!gcc % -o " .. output)                -- Compile the file
+		if vim.v.shell_error == 0 then
+			vim.cmd("belowright 13split | terminal " .. output) -- Open a split terminal and run the output file
+		else
+			vim.api.nvim_echo({ { "Compilation failed!", "ErrorMsg" } }, false, {})
+		end
+	elseif vim.bo.filetype == "cpp" then
+		local output = vim.fn.expand("%:r")
+		vim.cmd("w")                                   -- Save the file
+		vim.cmd("!g++ % -o " .. output)                -- Compile the file
 		if vim.v.shell_error == 0 then
 			vim.cmd("belowright 13split | terminal " .. output) -- Open a split terminal and run the output file
 		else
