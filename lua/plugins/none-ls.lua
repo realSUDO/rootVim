f50480a7499d6b2a15abce083f76af161eb9837a
justmultiply <justmultiplythinks@gmail.com>
2024-07-11 02:14:32 +0530

    added gnu style on formatting c

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,9 +5,10 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.prettier,
-
+				null_ls.builtins.formatting.clang_format.with({
+				extra_args = {"--style=gnu" },
+				}),
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
