3f7e2c9f7088d977d2824ee9e94f4af020f147ac
justmultiply <justmultiplythinks@gmail.com>
2024-08-03 19:48:19 +0530

    Better style than GNU

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,14 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.clang_format.with({
-				extra_args = {"--style=gnu" },
+					extra_args = { "--style=file" }, --add .clang-format file in your home directory with your custom stylings or simply replace --style=file with --style=GNU
+					-- my custom style is
+
+					-- BasedOnStyle: LLVM
+					-- IndentWidth: 4
+					-- UseTab: Always
+					-- BreakBeforeBraces: Allman
+					-- AllowShortFunctionsOnASingleLine: InlineOnly
 				}),
 			},
 		})
