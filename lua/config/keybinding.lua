299affe38f8b93cffd731883333fee965e7239f4
justmultiply <justmultiplythinks@gmail.com>
2024-08-15 17:41:42 +0530

    Add Pane switch keybinding and made keybinding.lua file more goodlooking

--- lua/config/keybinding.lua
+++ lua/config/keybinding.lua
@@ -1,13 +1,28 @@
-vim.keymap.set("t", "<leader>tt", "exit <CR> ", {})
+--define a helper function to set keymapping..
+local function map(mode, lhs, rhs, opts)
+	--set defualt options
+	local options = { noremap = true, silent = true }
+	--for additional option .. merge if provided
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	--set keymapping using final option
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+--switching panes
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-l>", "<C-w>l")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-h>", "<C-w>h")
 
-vim.api.nvim_set_keymap(
-	"n",
-	"<leader><leader><leader>r",
-	":w<CR> | :lua CompileAndRun()<CR>",
-	{ noremap = true, silent = true }
-)
+--closing terminal
+map("t", "<leader>tt", "exit <CR> ", {})
 
+--compile and run
+map("n", "<leader><leader><leader>r", ":w<CR> | :lua CompileAndRun()<CR>")
 
---toggle terminal
-vim.api.nvim_set_keymap('n', '<leader>tt', ':lua OpenClearTerminal("horizontal")<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ty', ':lua OpenClearTerminal("vertical")<CR>', { noremap = true, silent = true })
+--open terminal horizontal
+map("n", "<leader>tt", ':lua OpenClearTerminal("horizontal")<CR>')
+
+--open terminal vertical
+map("n", "<leader>ty", ':lua OpenClearTerminal("vertical")<CR>')
