59769c56ab9d1f5b4a244e1aa4b0411e6dbe01b2
justmultiply <justmultiplythinks@gmail.com>
2024-08-16 07:02:50 +0530

    Seprate Direcotry for compiled files
    Create a new directory for c/c++ files under root dir of project or
    ..the folder which contains .git
    (if no .git it create compiledFiles in working directory)

--- lua/config/compileandrun.lua
+++ lua/config/compileandrun.lua
@@ -1,23 +1,52 @@
 function CompileAndRun()
-	if vim.bo.filetype == "c" then
-		local output = vim.fn.expand("%:r")
-		vim.cmd("w")                                   -- Save the file
-		vim.cmd("!gcc % -o " .. output)                -- Compile the file
-		if vim.v.shell_error == 0 then
-			vim.cmd("belowright 13split | terminal " .. output) -- Open a split terminal and run the output file
-		else
-			vim.api.nvim_echo({ { "Compilation failed!", "ErrorMsg" } }, false, {})
-		end
-	elseif vim.bo.filetype == "cpp" then
-		local output = vim.fn.expand("%:r")
-		vim.cmd("w")                                   -- Save the file
-		vim.cmd("!g++ % -o " .. output)                -- Compile the file
-		if vim.v.shell_error == 0 then
-			vim.cmd("belowright 13split | terminal " .. output) -- Open a split terminal and run the output file
-		else
-			vim.api.nvim_echo({ { "Compilation failed!", "ErrorMsg" } }, false, {})
-		end
-	elseif vim.bo.filetype == "javascript" or filetype == "typescript" then
-		vim.cmd("belowright 13split | terminal node %")
-	end
+    local function get_project_root()
+        local function is_git_repo(path)
+            return vim.fn.isdirectory(path .. "/.git") == 1
+        end
+
+        local current_dir = vim.fn.expand("%:p:h")
+        local root = current_dir
+
+        while root ~= "/" do
+            if is_git_repo(root) then
+                return root
+            end
+            root = vim.fn.fnamemodify(root, ":h")
+        end
+
+        return vim.fn.getcwd()
+    end
+
+    local function ensure_compiled_files_dir()
+        local project_root = get_project_root()
+        local compiled_files_dir = project_root .. "/compiledFiles"
+        if vim.fn.isdirectory(compiled_files_dir) == 0 then
+            vim.fn.mkdir(compiled_files_dir, "p")
+        end
+        return compiled_files_dir
+    end
+
+    local compiled_files_dir = ensure_compiled_files_dir()
+
+    if vim.bo.filetype == "c" then
+        local output = compiled_files_dir .. "/" .. vim.fn.expand("%:t:r")
+        vim.cmd("w")  -- Save the file
+        vim.cmd("!gcc % -o " .. output)  -- Compile the file
+        if vim.v.shell_error == 0 then
+            vim.cmd("belowright 13split | terminal " .. output)  -- Open a split terminal and run the output file
+        else
+            vim.api.nvim_echo({ { "Compilation failed!", "ErrorMsg" } }, false, {})
+        end
+    elseif vim.bo.filetype == "cpp" then
+        local output = compiled_files_dir .. "/" .. vim.fn.expand("%:t:r")
+        vim.cmd("w")  -- Save the file
+        vim.cmd("!g++ % -o " .. output)  -- Compile the file
+        if vim.v.shell_error == 0 then
+            vim.cmd("belowright 13split | terminal " .. output)  -- Open a split terminal and run the output file
+        else
+            vim.api.nvim_echo({ { "Compilation failed!", "ErrorMsg" } }, false, {})
+        end
+    elseif vim.bo.filetype == "javascript" or vim.bo.filetype == "typescript" then
+        vim.cmd("belowright 13split | terminal node %")
+    end
 end
