03a6f20c64c04fd0854a3a820e045df974df6dee
Sumit Vishwkarma <justmultiplythinks@gmail.com>
2025-08-26 14:07:48 +0530

    Tabs now work for sugggestion box

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -39,6 +39,8 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
